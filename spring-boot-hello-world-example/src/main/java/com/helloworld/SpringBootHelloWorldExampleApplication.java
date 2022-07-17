11433386f37bd0a0e6614d61a9b56472fabc1142
aman-kumr <aman.bhole@seniorworld.com>
2022-07-18 00:16:52 +0530

    prepare artifact

--- spring-boot-hello-world-example/src/main/java/com/helloworld/SpringBootHelloWorldExampleApplication.java
+++ spring-boot-hello-world-example/src/main/java/com/helloworld/SpringBootHelloWorldExampleApplication.java
@@ -2,20 +2,12 @@ package com.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootHelloWorldExampleApplication  extends SpringBootServletInitializer
+public class SpringBootHelloWorldExampleApplication
 {
-	@Override
-	
-	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-	
-	        return application.sources(SpringBootHelloWorldExampleApplication.class);
-	   }
 	public static void main(String[] args) 
-{
-SpringApplication.run(SpringBootHelloWorldExampleApplication.class, args);
-}
+	{
+		SpringApplication.run(SpringBootHelloWorldExampleApplication.class, args);
+	}
 }
--- spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
+++ spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
@@ -4,9 +4,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController 
 {
-@RequestMapping("/")
-public String hello() 
-{
-return "<h1> Congratulations. You have successfully deployed the sample Spring Boot Application. </h1>";
-}
+	@RequestMapping("/")
+	public String hello() 
+	{
+		return "<h1> Congratulations. You have successfully deployed the sample Spring Boot Application. </h1>";
+	}
 }
