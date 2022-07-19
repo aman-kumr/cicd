b94a91e60c69e09a21134e18d27d856c03a76903
aman-kumr <aman.bhole@seniorworld.com>
2022-07-20 04:17:25 +0530

    revision

--- spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
+++ spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
@@ -2,11 +2,11 @@ package com.helloworld.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
-public class HelloWorldController 
+public class HelloWorldController
 {
 	@RequestMapping("/")
-	public String hello() 
+	public String hello()
 	{
-		return "<h1> Congratulations. You have successfully deployed the sample Spring Boot Application. </h1>";
+		return "<h1> Congratulations. You have successfully deployed the sample Spring Boot Applications. </h1>";
 	}
 }
