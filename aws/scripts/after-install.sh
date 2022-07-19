3f722b8ac9f58e583dfa27359f7bd8184ca5b628
aman-kumr <aman.bhole@seniorworld.com>
2022-07-20 03:41:30 +0530

    fix path

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -5,5 +5,5 @@ set -xe
 # Copy jar file to correct directory
 echo "Setting executable previliges for all the scripts and required jars"
 chmod +x /home/ubuntu/app/*.jar
-chmod +x /home/ubuntu/app/*.sh
+chmod -R +x /home/ubuntu/app/scripts/*.sh
 echo "Done setting the previliges for needed scripts and jars"
