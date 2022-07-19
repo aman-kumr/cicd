d2ab46449b42ef75fc3dfea1e3b6204bffcb71d5
aman-kumr <aman.bhole@seniorworld.com>
2022-07-20 04:10:02 +0530

    app stop script fixed

--- aws/scripts/application-stop.sh
+++ aws/scripts/application-stop.sh
@@ -2,7 +2,8 @@
 set -x
 
 # Jps will return processId
-app_process=$(jps | grep SpringBootHello | awk '{print $1}')
+app_process=$(jps | grep artifact | awk '{print $1}')
 if [ "$app_process" ]; then
+    echo "Killing process $app_process"
     kill -9 $app_process
 fi
